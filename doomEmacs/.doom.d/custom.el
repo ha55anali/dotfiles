8bbab4488063f9a753277cbb5cca0c62446560e0
hassan <phelenon@gmail.com>
2021-01-15 03:24:43 +0500

    set emacs theme to gruvbox

--- doomEmacs/.doom.d/custom.el
+++ doomEmacs/.doom.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" "f7216d3573e1bd2a2b47a2331f368b45e7b5182ddbe396d02b964b1ea5c5dc27" default)))
+   '("6b80b5b0762a814c62ce858e9d72745a05dd5fc66f821a1c5023b4f2a76bc910" "8e959d5a6771b4d1e2177263e1c1e62c62c0f848b265e9db46f18754ea1c1998" "bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" "f7216d3573e1bd2a2b47a2331f368b45e7b5182ddbe396d02b964b1ea5c5dc27" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
